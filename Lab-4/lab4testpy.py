deca83690079264104392e64176298f900691715
mslok <mslokar18@gmail.com>
2020-10-08 09:03:46 -0400

    lab 4 files

---
+++ Lab-4/lab4testpy.py
@@ -0,0 +1,21 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+#code example from Device Plus Editorial Team
+
+while True:
+    orientation = sense.get_orientation()
+    p = round(orientation["pitch"], 0)
+    
+    r = round(orientation["roll"], 0)
+    
+    y = round(orientation["yaw"], 0)
+    
+    
+    print("p: %s, r: %s, y: %s" % (p,r,y))
+    print("moved")
+    
+    
+    time.sleep(1)
